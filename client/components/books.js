ca9c499bab6a06b9f8a7a2ede99dca48fa30ea8e
jeffpash <jeffypash@gmail.com>
2018-04-20 17:10:20 +0200

    working react front

--- client/components/books.js
+++ client/components/books.js
@@ -1,9 +1,82 @@
-import React from 'react';
+//import React from 'react';
 
-export default class Books extends React.Component {
-  render() {
-    return(
-      <h1>BOOKS</h1>
-    );
-  }
-};
+// export default class Books extends React.Component {
+//   render() {
+//     return(
+//       <h1>BOOKS</h1>
+//     );
+//   }
+// };
+
+
+
+  // constructor(props) {
+  //   super(props)
+  //   this.states = {}
+  // }
+
+  // componentDidMount() {
+  //   fetch('/api/books')
+  //   .then(res => res.json())
+  //   .then(books => this.setState({ books }));
+  // }
+  //
+  // ***********************************
+  //
+  // <h1>Books</h1>
+  // <ul>
+  //   {this.state.books.map(book =>
+  //     <li key={book.id}>{book.title}</li>
+  //   )}
+  // </ul>
+
+  // <div className="navbar-wrapper">
+  //   <nav className="navbar navbar-default">
+  //     <div className="container">
+  //       <div className="navbar-header">
+  //         <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+  //           <span className="sr-only">Toggle navigation</span>
+  //           <span className="icon-bar"></span>
+  //           <span className="icon-bar"></span>
+  //           <span className="icon-bar"></span>
+  //         </button>
+  //         <a className="navbar-brand" href="#">Bookstore</a>
+  //         <a className="navbar-brand" href="#">Books</a>
+  //         <a className="navbar-brand" href="#">Genres</a>
+  //       </div>
+  //       <div id="navbar" className="collapse navbar-collapse">
+  //         <ul className="nav navbar-nav navbar-right">
+  //           <li>
+  //             <a href="#/books/add">Add Book</a>
+  //             <button type="button" className="btn btn-success">Sign in</button>
+  //             <button type="button" className="btn btn-danger">Sign up</button>
+  //           </li>
+  //         </ul>
+  //       </div>
+  //     </div>
+  //   </nav>
+  //
+  //   <div>{Books}</div>
+  //
+  //   <div className="container">
+  //     <div className="row">
+  //       <div className="col-md-12">
+  //         <h1>coming soonnn!!!.......</h1>
+  //         <div className="cont">
+  //         <div className="test"><img src="/book.jpg" alt="#" /></div>
+  //         <div className="test"></div>
+  //         <div className="test"></div>
+  //         <div className="test"></div>
+  //         <div className="test"></div>
+  //         <div className="test"></div>
+  //         <div className="test"></div>
+  //         <div className="test"></div>
+  //         <div className="test"></div>
+  //         <div className="test"></div>
+  //         </div>
+  //
+  //       </div>
+  //     </div>
+  //   </div>
+  //
+  // </div>
